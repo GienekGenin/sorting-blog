b2f4093c5dcf11f27a9de1fe1790e8e0cb3003de
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-08 17:21:25 +0200

    merge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import $ from 'jquery';
 import './scss/style.scss';
+
+$(document).ready(function(){
+    //hamburger menu functionality
+    $('.menu-trigger').click(function(){
+        $('header ul').slideToggle(1000);
+    });
+    $(window).resize(function(){
+        if ($(window).width() > 780 ) {
+            $('header ul').removeAttr('style');
+        }
+    });
+});
